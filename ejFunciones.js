a4e17bd3331af648be7147cbd2a9280342e59ce7
Aldana Lafuente <44821947+aldilafuente@users.noreply.github.com>
2019-02-25 12:39:57 -0300

    Ejercicios

---
+++ ejFunciones.js
@@ -0,0 +1,32 @@
+// EJERCICIOS 3, 6 Y 8
+
+
+// EJERCICIO 3
+// var mostrarNombre = function() {
+//     console.log('==========');
+//     console.log('= Aldana =');
+//     console.log('==========');
+// }
+// mostrarNombre();
+// mostrarNombre();
+
+
+// EJERCICIO 6
+// function obtenerNombre () {
+//     return 'Aldi'
+// }
+// console.log('Mi nombre es', obtenerNombre());
+
+
+// EJERCICIO 8
+// function obtenerNombre () {
+//     return 'Aldi!'
+// }
+// function obtenerSaludo() {
+//     var nombre = obtenerNombre();
+//     return '¡Hola, ' + nombre;
+// }
+// console.log(obtenerSaludo());
+
+
+
